90468f8a2b025579b337de8f4440ab06555f8d71
DucMinh <116259294+minh12200412@users.noreply.github.com>
2024-06-01 23:36:03 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
-const nameGirl = 'Gửi tới Khánhh';
-const giftUrl = '';
-const eventName = 'Chúc Mừng Sinh Nhật 02-06';
-const titleCard = 'Chúc Mừng tuổi 19';
+const nameGirl = 'Chúc Mừng Sinh Nhật 02-06';
+const giftUrl = 'https://www.facebook.com/Kapwpy';
+const eventName = 'Gửi tới Khánhh';
+const titleCard = 'Chúc Mừng Tuổi 19';
 const contentCard = 'Chúc em có một ngày sinh nhật thật ý nghĩa và nhiều niềm vui. Mong điều đẹp nhất sẽ đến với em ở tuổi 19, hãy luôn giữ nụ cười trên môi';
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
